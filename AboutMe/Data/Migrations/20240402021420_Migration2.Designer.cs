049d512338ef96f2b9c7863ac6a628aab1ce3d19
Theo Arnold <arnoldth@cardiff.ac.uk>
2024-04-03 19:33:52 +0100

    Simplifying login system

--- AboutMe/Data/Migrations/20240402021420_Migration2.Designer.cs
+++ AboutMe/Data/Migrations/20240402021420_Migration2.Designer.cs
@@ -4,6 +4,7 @@ using AboutMe.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AboutMe.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240402021420_Migration2")]
+    partial class Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
